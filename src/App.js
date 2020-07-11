279b80d7124381987cdf57ef7af3c5bbf15bddc3
Garrett Tolbert <garrett1.tolbert@gmail.com>
2020-07-11 16:37:30 -0500

    added react router

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
 import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Feed from './components/organisms/Feed';
+import Login from './components/organisms/Login';
 import db from './config';
 
 function App() {
-  
-	return <div className='App'>hi</div>;
+	return (
+		<Router>
+			<Switch>
+				<Route path='/feed'>
+					<Feed />
+				</Route>
+				<Route path='/'>
+					<Login />
+				</Route>
+			</Switch>
+		</Router>
+	);
 }
 
 export default App;
